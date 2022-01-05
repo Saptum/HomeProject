b0b9b723bf84486d1f406e2f00f9c2660e382545
Your Name <you@example.com>
2022-01-06 00:29:25 +0200

    Work with class Book

--- src/main/java/com/example/homeproject/model/Book.java
+++ src/main/java/com/example/homeproject/model/Book.java
@@ -1,4 +1,19 @@
 package com.example.homeproject.model;
 
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Data
+@Entity
 public class Book {
+    @Id
+    private Long id;
+    private String name;
+    private int pages;
+    private String author;
+
+
+
 }
