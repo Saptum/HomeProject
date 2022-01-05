5d1fff83211c4fa1cf95c849342c951da3057230
Your Name <you@example.com>
2022-01-06 00:37:47 +0200

    Work with class service

--- src/main/java/com/example/homeproject/service/BookService.java
+++ src/main/java/com/example/homeproject/service/BookService.java
@@ -1,4 +1,31 @@
 package com.example.homeproject.service;
 
+import com.example.homeproject.model.Book;
+import com.example.homeproject.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class BookService {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    public Book saveBook(Book book){
+        return bookRepository.save(book);
+    }
+
+    public List<Book> saveBooks(List<Book> bookList){
+        return bookRepository.saveAll(bookList);
+    }
+
+    public List<Book> findAllBooks(List<Book> bookList){
+        return bookRepository.findAll();
+    }
+
+
+
+
 }
